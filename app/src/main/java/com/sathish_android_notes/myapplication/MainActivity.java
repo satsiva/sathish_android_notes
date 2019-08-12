59314d01a1674caac912bf622014d08bdf9528b9
satsiva <satcatbat@gmail.com>
2019-08-12 13:07:04 +0530

    sample

---
+++ app/src/main/java/com/sathish_android_notes/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sathish_android_notes.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+//add git using this link
+//https://stackoverflow.com/questions/37093723/how-to-add-an-android-studio-project-to-github
